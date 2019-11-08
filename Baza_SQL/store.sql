c01b654e71fe96bde91c29d4d6cd1a3a790d6bb0
Tadeusz Kantor <smigolpol@gmail.com>
2019-11-08 12:40:35 +0100

    Dodanie pliku bazy danych.

---
+++ Baza_SQL/store.sql
@@ -0,0 +1,268 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Czas generowania: 08 Lis 2019, 12:39
+-- Wersja serwera: 10.1.37-MariaDB
+-- Wersja PHP: 7.3.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Baza danych: `store`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `access_levels`
+--
+
+CREATE TABLE `access_levels` (
+  `id` tinyint(1) UNSIGNED NOT NULL,
+  `name` tinytext COLLATE utf8_polish_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
+
+--
+-- Zrzut danych tabeli `access_levels`
+--
+
+INSERT INTO `access_levels` (`id`, `name`) VALUES
+(0, 'USER'),
+(1, 'ADMIN');
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `orders`
+--
+
+CREATE TABLE `orders` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `user` int(10) UNSIGNED NOT NULL,
+  `date` date NOT NULL,
+  `state` tinyint(1) UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
+
+--
+-- Zrzut danych tabeli `orders`
+--
+
+INSERT INTO `orders` (`id`, `user`, `date`, `state`) VALUES
+(1, 0, '2019-11-08', 3);
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `order_contents`
+--
+
+CREATE TABLE `order_contents` (
+  `order_id` int(10) UNSIGNED NOT NULL,
+  `product_id` int(10) UNSIGNED NOT NULL,
+  `quantity` smallint(5) UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
+
+--
+-- Zrzut danych tabeli `order_contents`
+--
+
+INSERT INTO `order_contents` (`order_id`, `product_id`, `quantity`) VALUES
+(1, 1, 3);
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `order_states`
+--
+
+CREATE TABLE `order_states` (
+  `id` tinyint(1) UNSIGNED NOT NULL,
+  `name` varchar(16) COLLATE utf8_polish_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
+
+--
+-- Zrzut danych tabeli `order_states`
+--
+
+INSERT INTO `order_states` (`id`, `name`) VALUES
+(0, 'PENDING'),
+(1, 'SENT'),
+(2, 'FINISHED'),
+(3, 'CANCELLED');
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `product_info`
+--
+
+CREATE TABLE `product_info` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `name` varchar(64) COLLATE utf8_polish_ci NOT NULL,
+  `description` text COLLATE utf8_polish_ci NOT NULL,
+  `price` decimal(65,2) UNSIGNED NOT NULL,
+  `supplier` varchar(64) COLLATE utf8_polish_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
+
+--
+-- Zrzut danych tabeli `product_info`
+--
+
+INSERT INTO `product_info` (`id`, `name`, `description`, `price`, `supplier`) VALUES
+(1, 'Tester kabli', 'abc', '123.45', 'QWERTY');
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `stock`
+--
+
+CREATE TABLE `stock` (
+  `product_id` int(10) UNSIGNED NOT NULL,
+  `quantity` smallint(5) UNSIGNED NOT NULL DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
+
+--
+-- Zrzut danych tabeli `stock`
+--
+
+INSERT INTO `stock` (`product_id`, `quantity`) VALUES
+(1, 5);
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `username` varchar(16) COLLATE utf8_polish_ci NOT NULL,
+  `password_hash` char(32) CHARACTER SET ascii COLLATE ascii_bin NOT NULL,
+  `access_level` tinyint(3) UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
+
+--
+-- Zrzut danych tabeli `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `password_hash`, `access_level`) VALUES
+(0, 'admin', '21232f297a57a5a743894a0e4a801fc3', 1);
+
+--
+-- Indeksy dla zrzutów tabel
+--
+
+--
+-- Indeksy dla tabeli `access_levels`
+--
+ALTER TABLE `access_levels`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeksy dla tabeli `orders`
+--
+ALTER TABLE `orders`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user` (`user`),
+  ADD KEY `state` (`state`);
+
+--
+-- Indeksy dla tabeli `order_contents`
+--
+ALTER TABLE `order_contents`
+  ADD KEY `order_id` (`order_id`),
+  ADD KEY `product_id` (`product_id`);
+
+--
+-- Indeksy dla tabeli `order_states`
+--
+ALTER TABLE `order_states`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeksy dla tabeli `product_info`
+--
+ALTER TABLE `product_info`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeksy dla tabeli `stock`
+--
+ALTER TABLE `stock`
+  ADD UNIQUE KEY `product_id` (`product_id`);
+
+--
+-- Indeksy dla tabeli `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `access_level` (`access_level`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT dla tabeli `orders`
+--
+ALTER TABLE `orders`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT dla tabeli `product_info`
+--
+ALTER TABLE `product_info`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT dla tabeli `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- Ograniczenia dla zrzutów tabel
+--
+
+--
+-- Ograniczenia dla tabeli `orders`
+--
+ALTER TABLE `orders`
+  ADD CONSTRAINT `orders_ibfk_1` FOREIGN KEY (`user`) REFERENCES `users` (`id`),
+  ADD CONSTRAINT `orders_ibfk_2` FOREIGN KEY (`state`) REFERENCES `order_states` (`id`);
+
+--
+-- Ograniczenia dla tabeli `order_contents`
+--
+ALTER TABLE `order_contents`
+  ADD CONSTRAINT `order_contents_ibfk_1` FOREIGN KEY (`order_id`) REFERENCES `orders` (`id`),
+  ADD CONSTRAINT `order_contents_ibfk_2` FOREIGN KEY (`product_id`) REFERENCES `product_info` (`id`);
+
+--
+-- Ograniczenia dla tabeli `stock`
+--
+ALTER TABLE `stock`
+  ADD CONSTRAINT `stock_ibfk_1` FOREIGN KEY (`product_id`) REFERENCES `product_info` (`id`);
+
+--
+-- Ograniczenia dla tabeli `users`
+--
+ALTER TABLE `users`
+  ADD CONSTRAINT `users_ibfk_1` FOREIGN KEY (`access_level`) REFERENCES `access_levels` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
