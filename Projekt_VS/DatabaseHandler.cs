24d47ea1afbeba5bcf71944673d4f56b65b6fc7f
ignis05 <48099798+ignis05@users.noreply.github.com>
2019-11-20 20:38:32 +0100

    Naprawiona baza danych

--- Projekt_VS/DatabaseHandler.cs
+++ Projekt_VS/DatabaseHandler.cs
@@ -8,9 +8,9 @@ namespace as_webforms_sklep
     {
         private const string connString =
                "SERVER=inf16.tl.krakow.pl;" +
-               "DATABASE=gmikolajczyk;" +
-               "UID=gmikolajczyk;" +
-               "PASSWORD=gmikolajczyk;";
+               "DATABASE=tkantor;" +
+               "UID=tkantor;" +
+               "PASSWORD=tkantor;";
 
         private static MySqlConnection connect()
         {
