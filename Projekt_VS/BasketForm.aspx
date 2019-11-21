2ce02d673f54129e9a4f11b2bb3abe00e5999861
LittleBigKiller <35974496+LittleBigKiller@users.noreply.github.com>
2019-11-21 22:42:22 +0100

    BasketForm, podsumowanie i edycja ilości, usuwanie z koszyka

---
+++ Projekt_VS/BasketForm.aspx
@@ -0,0 +1,47 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="BasketForm.aspx.cs" Inherits="as_webforms_sklep.BasketForm" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style type="text/css">
+        .auto-style1 {
+            width: 100%;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <table class="auto-style1">
+                <tr>
+                    <td>
+                        <asp:Label ID="lTotalPrice" runat="server" Text="Cena wszystkich przedmiotów w koszyku to:"></asp:Label>
+                    </td>
+                    <td>&nbsp;</td>
+                    <td>&nbsp;</td>
+                </tr>
+                <tr>
+                    <td>
+                        <asp:LinkButton ID="lbToMain" runat="server" PostBackUrl="~/MainForm.aspx">Wróć do sklepu</asp:LinkButton>
+                    </td>
+                    <td>&nbsp;</td>
+                    <td>&nbsp;</td>
+                </tr>
+            </table>
+        </div>
+        <asp:Repeater ID="rBasket" runat="server" OnItemCommand="basketHandler">
+            <ItemTemplate>
+                <div style="border: 2px solid black">
+                    <p><%# Eval("productId") %></p>
+                    <asp:TextBox ID="tbAmount" runat="server" type="number" value='<%# Eval("amount") %>' min="1" step="1"></asp:TextBox>
+                    <br />
+                    <asp:Button ID="bChangeProduct" CommandName="changeInBasket" CommandArgument='<%# Eval("productId") %>' runat="server" Text="Zmień ilość" />
+                    <asp:Button ID="bRemoveProduct" CommandName="removeFromBasket" CommandArgument='<%# Eval("productId") %>' runat="server" Text="Usuń koszyka" />
+                </div>
+            </ItemTemplate>
+        </asp:Repeater>
+    </form>
+</body>
+</html>
---
+++ Projekt_VS/BasketForm.aspx.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace as_webforms_sklep
+{
+    public partial class BasketForm : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Session["basket"] == null)
+            {
+                Debug.WriteLine("Create new basket");
+                Session["basket"] = new List<BasketItem>();
+            }
+
+            if (!IsPostBack)
+            {
+                rBasket.DataSource = (List<BasketItem>)Session["basket"];
+                rBasket.DataBind();
+            }
+
+            calculateTotalPrice();
+        }
+
+        protected void calculateTotalPrice()
+        {
+            List<BasketItem> basketList;
+            if (Session["basket"] == null)
+            {
+                basketList = new List<BasketItem>();
+            }
+            else
+            {
+                basketList = (List<BasketItem>)Session["basket"];
+            }
+
+            int totalPrice = 0;
+            foreach (BasketItem basketItem in basketList)
+            {
+                totalPrice += basketItem.amount ; // * basketItem.price;
+            }
+
+            lTotalPrice.Text = "Cena wszystkich przedmiotów w koszyku to: " + totalPrice.ToString();
+        }
+
+        protected void basketHandler(object source, RepeaterCommandEventArgs e)
+        {
+            if (e.CommandName == "changeInBasket")
+            {
+                Debug.WriteLine("changeInBasket");
+                List<BasketItem> basketList;
+                if (Session["basket"] == null)
+                {
+                    basketList = new List<BasketItem>();
+                }
+                else
+                {
+                    basketList = (List<BasketItem>)Session["basket"];
+                }
+
+                BasketItem basketItem = basketList.Find(item => item.productId == (e.CommandArgument.ToString()));
+
+                int amountToSet = 0;
+                TextBox tbAmount = (TextBox)e.Item.FindControl("tbAmount");
+                try
+                {
+                    amountToSet = int.Parse(tbAmount.Text);
+                }
+                catch (FormatException)
+                {
+                    amountToSet = basketItem.amount;
+                }
+
+                basketItem.amount = amountToSet;
+
+                calculateTotalPrice();
+            }
+            else if (e.CommandName == "removeFromBasket")
+            {
+                Debug.WriteLine("removeFromBasket");
+                List<BasketItem> basketList;
+                if (Session["basket"] == null)
+                {
+                    basketList = new List<BasketItem>();
+                }
+                else
+                {
+                    basketList = (List<BasketItem>)Session["basket"];
+                }
+
+                BasketItem basketItem = basketList.Find(item => item.productId == (e.CommandArgument.ToString()));
+
+                basketList.Remove(basketItem);
+                rBasket.DataSource = basketList;
+                rBasket.DataBind();
+
+                calculateTotalPrice();
+            }
+        }
+    }
+}
